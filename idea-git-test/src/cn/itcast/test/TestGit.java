8522d99a5f12ba03a248575cb18f1d2994a3a4da
admin <admin@itcast.cn>
2019-06-13 10:37:54 +0800

    添加test工程至Git

---
+++ idea-git-test/src/cn/itcast/test/TestGit.java
@@ -0,0 +1,12 @@
+package cn.itcast.test;
+
+public class TestGit {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+}
